6a4e888ada6f24f21d4c759360cb08fee3bc6075
Dan Trickey <dan@trickey.io>
2018-10-09 21:15:57 +0100

    Linting + README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# damm32
+# Damm32
 Python implementation of the Damm Algorithm in Base 32
+
+By default, it uses an alphabet as specified in [RFC 4648](https://tools.ietf.org/html/rfc4648) which contains 32 alphanumeric characters, with similar looking characters removed. The padding symbol is not included.
--- damm32/checksum.py
+++ damm32/checksum.py
@@ -5,20 +5,20 @@ from typing import List
 Digit = int
 DigitList = List[Digit]
 
+
 class Damm32:
     """Implementation of the Damm algorithm."""
 
     BASE_SIZE = 32
-    ALPHABET =  ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','2','3','4','5','6','7']
+    ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '2', '3', '4', '5', '6', '7']  # noqa: E501
 
     def _calculate_from_digits(self, digits: DigitList) -> Digit:
-        """Calculate the check digit from a list of integers."""
         mask = 37
         checkdigit = 0
         for digit in digits:
             checkdigit ^= digit
             checkdigit <<= 1
-            if checkdigit >= Damm32.BASE_SIZE: 
+            if checkdigit >= Damm32.BASE_SIZE:
                 checkdigit ^= mask
         return checkdigit
 
@@ -32,13 +32,15 @@ class Damm32:
     def _to_word(self, digits: DigitList) -> str:
         word = ""
         for i in digits:
-            word.append(Damm32.ALPHABET[i])
+            word += Damm32.ALPHABET[i]
         return word
 
     def calculate(self, word: str) -> str:
+        """Calculate the check digit for a string."""
         digits = self._to_digits(word)
         checkdigit = self._calculate_from_digits(digits)
         return Damm32.ALPHABET[checkdigit]
 
     def verify(self, word: str) -> bool:
+        """Verify that a string contains a valid check digit."""
         return self.calculate(word) == Damm32.ALPHABET[0]
