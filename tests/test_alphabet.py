640911612a6d73a2ac08101851a700b58ad14151
Dan Trickey <dan@trickey.io>
2019-03-15 01:20:26 +0000

    Reformat test_alphabet and unignore linting.

--- tests/test_alphabet.py
+++ tests/test_alphabet.py
@@ -7,40 +7,167 @@ from damm32.checksum import BadAlphabetException, BadCharacterException
 
 def test_default_alphabet():
     """Test that the default alphabet does not throw an error."""
-    d32 = Damm32()  # noqa: F841
+    Damm32()
 
 
 def test_empty_alphabet():
     """Test that an empty alphabet does throw an error."""
     with pytest.raises(BadAlphabetException):
-        d32 = Damm32([])  # noqa: F841
+        Damm32([])
 
 
 def test_valid_alphabet():
     """Test that a valid alphabet does not throw an error."""
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '2', '3', '4', '5', '6', '7']  # noqa: E501
-    d32 = Damm32(alphabet)  # noqa: F841
+    alphabet = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+    ]
+    Damm32(alphabet)
 
 
 def test_short_alphabet():
     """Test that a short alphabet does throw an error."""
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X']  # noqa: E501
+    alphabet = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+    ]
     with pytest.raises(BadAlphabetException):
-        d32 = Damm32(alphabet)  # noqa: F841
+        Damm32(alphabet)
 
 
 def test_long_alphabet():
     """Test that a long alphabet does throw an error."""
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '2', '3', '4', '5', '6', '7', '?', '@', '=']  # noqa: E501
+    alphabet = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "?",
+        "@",
+        "=",
+    ]
     with pytest.raises(BadAlphabetException):
-        d32 = Damm32(alphabet)  # noqa: F841
+        Damm32(alphabet)
 
 
 def test_duplicate_alphabet():
     """Test that an alphabet with duplicates does throw an error."""
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '2', '3', '4', '5', '6', '6']  # noqa: E501
+    alphabet = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "6",
+    ]
     with pytest.raises(BadAlphabetException):
-        d32 = Damm32(alphabet)  # noqa: F841
+        Damm32(alphabet)
 
 
 def test_bad_letter_detected():
