b0a0f5101abadadf77c3370464a3e68eaa089c6b
Dan Trickey <dan@trickey.io>
2020-08-20 20:34:14 +0100

    Bump version number to 1.2.0

--- damm32/__init__.py
+++ damm32/__init__.py
@@ -8,3 +8,5 @@ A python implementation of the Damm algorithm, in base 32.
 from .checksum import BadAlphabetException, BadCharacterException, Damm32
 
 __all__ = ["BadAlphabetException", "BadCharacterException", "Damm32"]
+
+__version__ = "1.2.0"
